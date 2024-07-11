1d27cc42424e1a7381c8e200dafb4c531223be4a
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-07-11 13:48:11 +0000

    <create> sql: unique users

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,7 +2,7 @@
 -- attr id, email, name
 
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY_KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email CHAR(255) NOT NULL UNIQUE,
     name CHAR(255)
 );

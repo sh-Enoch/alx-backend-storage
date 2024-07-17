12a4f94d97e62d3a0df1b28bef2efcf239d0c393
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-07-17 00:08:27 +0000

    <create> function: update_topics

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -5,5 +5,5 @@ import pymongo
 
 def update_topics(mongo_collection, name, topics):
     """Change all topics of a school document based on the name."""
-    new_doc = mongo_collection.update({"name": name},
-                                      {"$set": {"topics": topics}})
+    new_doc = mongo_collection.update_many({"name": name},
+                                           {"$set": {"topics": topics}})
